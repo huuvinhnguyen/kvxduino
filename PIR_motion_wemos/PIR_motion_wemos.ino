cc6daeadc7220394d2e181ba817edbaedf118f92
Vinh Nguyen <huuvinh@live.com>
2018-11-13 16:02:21 +0700

    Pir motion with eeprom

--- PIR_motion_wemos/PIR_motion_wemos.ino
+++ PIR_motion_wemos/PIR_motion_wemos.ino
@@ -1,26 +1,108 @@
 #include <WiFiManager.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include "DataDefault.h"
+#include "ViewInteractor.h"
 
 int pirPin = D7;
 int val;
 
-void setup()
-{
+struct Configuration {
+  char trigger[50];
+  char key[100];
+} configuration;
+
+ESP8266WebServer server(80);
+
+void setup() {
+
   Serial.begin(115200);
   setupWiFi();
+  setupAliasHost();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
 
+  loadDataDefault();
+
+  ViewInteractor viewInteractor;
+  viewInteractor.lookupFiles();
+
+  server.onNotFound([]() {
+
+    ViewInteractor viewInteractor;
+    String path = server.uri();
+    if (!viewInteractor.isFileRead(path))
+
+      server.send(404, "text/plain", "FileNotFound");
+    else {
+
+      File file = viewInteractor.getFileRead(path);
+      size_t sent = server.streamFile(file, viewInteractor.getContentType(path));
+      file.close();
+    }
+  });
+
+
+  server.on("/setting", []() {
+
+    String trigger = server.arg("trigger");
+    Serial.println(trigger);
+    String key = server.arg("key");
+    Serial.println(key);
+    strcpy( configuration.trigger, trigger.c_str());
+    strcpy( configuration.key, key.c_str());
+    DataDefault<Configuration> dataDefault;
+    dataDefault.saveObject(configuration);
+
+    String content = "<html>";
+    content += "<body>";
+    content += "<h2>Your Trigger:  <h1>" ;
+    content += trigger ;
+    content += "</h1>";
+    content += "has been saved.";
+    content += "</h2>" ;
+    content += "</body>";
+    content += "</html>";
+    server.send(200, "text/html", content);
+  });
+
+  server.on("/vieweeprom", []() {
+
+    DataDefault<Configuration> dataDefault;
+    Configuration configuration = dataDefault.loadObject();
+
+    Serial.println( "Read custom object from EEPROM: " );
+    Serial.println( configuration.trigger );
+    Serial.println( configuration.key);
+
+    String content = "<html>";
+    content += "<body>";
+    content += "<h2>Your trigger:  <h1>" ;
+    content += configuration.trigger ;
+    content += "</h1>";
+    content += configuration.key;
+    content += "</h2>" ;
+    content += "</body>";
+    content += "</html>";
+
+    server.send(200, "text/html", content);
+  });
+
+  server.begin();
 }
 
-void loop()
-{
+void loop() {
+
+  server.handleClient();
+
   val = digitalRead(pirPin);
   //low = no motion, high = motion
-  if (val == LOW)
-  {
+  if (val == LOW) {
     Serial.println("No motion");
-  }
-  else
-  {
+  } else {
     Serial.println("Motion detected  ALARM");
     alertMotion();
   }
@@ -29,8 +111,8 @@ void loop()
 }
 
 void alertMotion() {
-  delay(5000);
 
+  delay(5000);
   Serial.print("connecting to ");
   const char* host = "maker.ifttt.com";
   Serial.println(host);
@@ -44,7 +126,10 @@ void alertMotion() {
   }
 
   // We now create a URI for the request
-  String url = "/trigger/rain_alert/with/key/d9pfo19I0z65wZiQVhGgUO";
+  String url = "/trigger/";
+  url += configuration.trigger;
+  url +=  "/with/key/";
+  url += configuration.key;
 
   Serial.print("Requesting URL: ");
   Serial.println(url);
@@ -70,6 +155,20 @@ void setupWiFi() {
 
   delay(10000);
   WiFiManager wifiManager;
-  wifiManager.setConfigPortalTimeout(60);
+  wifiManager.setConfigPortalTimeout(30);
   wifiManager.startConfigPortal();
+}
+
+void setupAliasHost() {
+
+  const char* host = "khuonvienxanh";
+  MDNS.begin(host);
+  MDNS.addService("http", "tcp", 80);
+  MDNS.addService("ws", "tcp", 81);
+}
+
+void loadDataDefault() {
+
+  DataDefault<Configuration> dataDefault;
+  configuration = dataDefault.loadObject();
 }
--- esp_cloudmqtt/esp_cloudmqtt.ino
+++ esp_cloudmqtt/esp_cloudmqtt.ino
@@ -1,96 +1,161 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+ESP8266WebServer server(80);
 
 #define TouchSensor 4 // Pin for capactitive touch sensor
 boolean currentState = LOW;
 boolean lastState = LOW;
 boolean RelayState = LOW;
- 
+
 const char* ssid = "Ving";
-const char* password =  "123456789";
-const char* mqttServer = "m11.cloudmqtt.com";
-const int mqttPort = 16242;
-const char* mqttUser = "mntdttex";
-const char* mqttPassword = "730VZPmi41Fd";
- 
+const char* password =  "1234567890";
+const char* mqttServer = "m15.cloudmqtt.com";
+const int mqttPort = 11692;
+const char* mqttUser = "quskfiwf";
+const char* mqttPassword = "HKfqtBl47aBR";
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 uint8_t ledPin = 2;
- 
+
 void setup() {
- 
+
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   pinMode(TouchSensor, INPUT);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
+  setupWiFi();
+//    WiFi.begin(ssid, password);
+  
+  
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+    }
+
   Serial.println("Connected to the WiFi network");
- 
+
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
- 
+
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
- 
+
     if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
- 
-      Serial.println("connected");  
- 
+
+      Serial.println("connected");
+
     } else {
- 
+
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
+
     }
   }
- 
+
   client.publish("esp/test", "Hello from ESP8266");
   client.subscribe("esp/test", 1);
- 
+
+  server.on("/vieweeprom", []() {
+
+
+    Serial.println( "Read custom object from EEPROM: " );
+
+
+    String content = "<html>";
+    content += "<body>";
+    content += "<h2>Your server:  <h1>" ;
+    //    content += configuration.server ;
+    content += "</h1>";
+    //    content += configuration.user;
+    content += "</h2>" ;
+    content += "</body>";
+    content += "</html>";
+
+    server.send(200, "text/html", content);
+  });
+
+  server.on("/setupwifi", []() {
+
+    WiFi.begin(ssid, "123456789");
+
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+    }
+
+
+    server.send(200, "text/html", "Setup Wifi");
+  });
+
+  server.begin();
+
 }
- 
+
 void callback(char* topic, byte* payload, unsigned int length) {
- 
+
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
+
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
- 
+
   Serial.println();
   Serial.println("-----------------------");
-  digitalWrite(ledPin,!digitalRead(ledPin));
- 
+  digitalWrite(ledPin, !digitalRead(ledPin));
+
 }
- 
+
 void loop() {
   client.loop();
 
-   currentState = digitalRead(TouchSensor);
-    if (currentState == HIGH && lastState == LOW){
+  currentState = digitalRead(TouchSensor);
+  if (currentState == HIGH && lastState == LOW) {
     Serial.println("pressed");
     delay(1);
 
-    if (RelayState == HIGH){
+    if (RelayState == HIGH) {
       digitalWrite(ledPin, LOW);
       RelayState = LOW;
       client.publish("esp/test", "Hello from LOW");
-      
+
     } else {
       digitalWrite(ledPin, HIGH);
-            client.publish("esp/test", "Hello from HIGH");
+      client.publish("esp/test", "Hello from HIGH");
 
       RelayState = HIGH;
     }
   }
   lastState = currentState;
+
+  server.handleClient();
+}
+
+void setupWiFi() {
+
+//  const char* ssid = "khuonvienxanh";
+//
+//  WiFi.softAP(ssid, "123456789");
+//  IPAddress myIP = WiFi.softAPIP();
+//  Serial.print("AP IP address: ");
+//  Serial.println(myIP);
+
+    delay(10000);
+    WiFiManager wifiManager;
+    wifiManager.setConfigPortalTimeout(60);
+    wifiManager.startConfigPortal();
+  
+//    const char* host = "khuonvienxanh";
+//     MDNS.begin(host);
+//       // Add service to MDNS
+//    MDNS.addService("http", "tcp", 80);
+//    MDNS.addService("ws", "tcp", 81);
 }
